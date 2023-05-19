7c13d8006b760c149ac122770ea5bbf16e7a7a3e
Marija Rubil <rubilmarija@gmail.com>
2023-05-19 14:49:40 +0200

    Created wave

--- src/app.js
+++ src/app.js
@@ -1,49 +1,76 @@
+import * as dat from "dat.gui";
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const img = document.createElement('img');
-img.src = 'https://unsplash.com/photos/3vHN3vTnOPg';
+ctx.beginPath();
+ctx.moveTo(0, canvas.height / 2);
+
+const gui = new dat.GUI();
+
+const wave = {
+    y: canvas.height / 2,
+    length: 0.01,
+    amplitude: 100,
+    frequency: 0.01,
+};
+
+const strokeColor = {
+    h: 200,
+    s: 50, // saturation
+    l: 50, // lightnes
+};
+
+const background = {
+    r: 0,
+    g: 0,
+    b: 0,
+    a: 0.1,
+};
+
+const waveFolder = gui.addFolder('wave');
+waveFolder.add(wave, "y", 0, canvas.height);
+waveFolder.add(wave, "length", -0.01, 0.01);
+waveFolder.add(wave, "amplitude", -300, 300);
+waveFolder.add(wave, "frequency", -0.01, 1);
+waveFolder.open();
+
+const strokeColorFolder = gui.addFolder('stroke color');
+strokeColorFolder.add(strokeColor, "h", 0, 255);
+strokeColorFolder.add(strokeColor, "s", 0, 100);
+strokeColorFolder.add(strokeColor, "l", 0, 100);
+strokeColorFolder.open();
+
+const backgroundFolder = gui.addFolder('background color');
+backgroundFolder.add(background, "r", 0, 255);
+backgroundFolder.add(background, "g", 0, 255);
+backgroundFolder.add(background, "b", 0, 255);
+backgroundFolder.add(background, "a", 0, 1.0);
+backgroundFolder.open();
+
+let increment = wave.frequency;
 
 function draw() {
-    ctx.strokeStyle = 'red';
-    ctx.beginPath();
-    ctx.moveTo(canvas.width / 2, 0);
-    ctx.lineTo(canvas.width / 2, canvas.height);
-    ctx.stroke();
+    requestAnimationFrame(draw);
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = `rgba(${background.r}, ${background.g}, ${background.b}, ${background.a})`;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
     ctx.moveTo(0, canvas.height / 2);
-    ctx.lineTo(canvas.width, canvas.height / 2);
-    ctx.strokeStyle = 'green';
+    for (let i = 0; i < canvas.width; i++) {
+        ctx.lineTo(
+            i,
+            wave.y +
+            Math.sin(i * wave.length + increment) *
+            wave.amplitude *
+            Math.sin(increment)
+        );
+    }
     ctx.stroke();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(canvas.width, canvas.height);
-    ctx.moveTo(canvas.width, 0);
-    ctx.lineTo(0, canvas.height);
-    ctx.stroke();
-    ctx.beginPath(); //
-    ctx.rect(canvas.width / 2 - 250, canvas.height / 2 - 250, 500, 500);
-    ctx.strokeStyle = 'blue';
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.arc(canvas.width / 2, canvas.height / 2, 100, 0, 2 * Math.PI);
-    ctx.strokeStyle = 'purple';
-    ctx.stroke();
-
-    ctx.font = '100px Arial';
-    ctx.textAlign = 'center'; // start, end
-    ctx.textBaseline = 'middle'; // top, bottom
-    ctx.fillText('Hello World', canvas.width / 2, canvas.height / 2); // x and y of the text
-
-    ctx.drawImage(img, canvas.width / 2, 10, 10);
+    ctx.strokeStyle = `hsl(${Math.abs(strokeColor.h * Math.sin(increment))}, ${strokeColor.s}%, ${strokeColor.l}%)`;
+    increment += wave.frequency;
 }
 
 draw();
-
-window.addEventListener("resize", () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    draw();
-});
